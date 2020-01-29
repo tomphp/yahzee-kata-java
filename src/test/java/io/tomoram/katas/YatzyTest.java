caa1482fd472af088e328e4eac146725c2620238
Tom Oram <me@tomoram.io>
2020-01-29 13:09:30 +0000

    Kill surviving mutants

--- src/test/java/io/tomoram/katas/YatzyTest.java
+++ src/test/java/io/tomoram/katas/YatzyTest.java
@@ -72,6 +72,7 @@ public class YatzyTest {
         assertEquals(6, Yatzy.score_pair(3,4,3,5,6));
         assertEquals(10, Yatzy.score_pair(5,3,3,3,5));
         assertEquals(12, Yatzy.score_pair(5,3,6,6,5));
+        assertEquals(12, Yatzy.score_pair(5,6,6,3,5));
         assertEquals(0, Yatzy.score_pair(1,2,3,4,5));
     }
 
@@ -87,6 +88,7 @@ public class YatzyTest {
     {
         assertEquals(9, Yatzy.three_of_a_kind(3,3,3,4,5));
         assertEquals(15, Yatzy.three_of_a_kind(5,3,5,4,5));
+        assertEquals(15, Yatzy.three_of_a_kind(5,3,4,5,5));
         assertEquals(9, Yatzy.three_of_a_kind(3,3,3,3,5));
         assertEquals(9, Yatzy.three_of_a_kind(3,3,3,3,3));
         assertEquals(0, Yatzy.three_of_a_kind(1,2,3,4,5));
